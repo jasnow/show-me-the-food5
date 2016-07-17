308022ff36dd65c2496315ab2478ff309143d52e
Al Snow <jasnow@hotmail.com>
2016-07-17 10:00:56 -0400

    Added back enable_entension to db/schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20141027235356) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.float    "latitude"
     t.float    "longitude"
