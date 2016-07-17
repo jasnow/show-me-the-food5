2d9a24e4d64d119e3194fad8fdb0ce54facea0f9
Al Snow <jasnow@hotmail.com>
2016-07-17 09:58:33 -0400

    Upgraded to Ruby 2.4.0-preview1

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20141027235356) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "locations", force: true do |t|
+  create_table "locations", force: :cascade do |t|
     t.float    "latitude"
     t.float    "longitude"
     t.string   "name"
